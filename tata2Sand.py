fe904647e804838c79bb5eb323326bc4c5477a4d
Yohan Guého <yohan.gueho@icloud.com>
2022-03-14 14:36:55 +0100

    e

--- tata2Sand.py
+++ tata2Sand.py
@@ -9,7 +9,7 @@
 
 ### MODULES ###
 
-from tkinter import *
+import tkinter as tk
 import random as rd
 import time
 
@@ -22,14 +22,29 @@ width = 500
 
 ### PROGRAMME ###
 
-root = Tk()
+def close():
+    root.destroy()
+
+def ecoulement(ecoulement):
+    i = 0
+    for i in range (0,20):
+        text="grain"
+        ecoulement = tk.Label(root, text=text)
+        ecoulement.pack()
+        i += 1
+
+root = tk.Tk()
+canvas = tk.Canvas(root, height=height, width=width)
 root.title("tata 2 sable")
 root.geometry('{}x{}'.format(height,width))
-canvas = Canvas(root, height=height, width=width)
-label = Label(root, text="Reconfiguration")
-bouton = Button(root, text="Go!")
 
-label.grid(row=0)
-bouton.grid(row=1)
+label = tk.Label(root, text="TATA 2 SABLE")
+label.pack(anchor=tk.CENTER)
+
+bouton = tk.Button(root, text="Go!")
+bouton.pack(anchor=tk.CENTER, side=tk.BOTTOM)
+
+Bclose = tk.Button(root, text="Close", command=close)
+Bclose.pack(anchor=tk.CENTER, side=tk.BOTTOM)
 
 root.mainloop()
